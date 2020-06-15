427f2d40f371c3645999f6e73269fb86627732e8
Mark Pekun <markpekun@Marks-MacBook-Pro.local>
2020-06-15 15:13:28 -0400

    imported browser router

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import { Router } from "express";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+<App />;
+ReactDOM.render(<Router></Router>, document.getElementById("root"));
