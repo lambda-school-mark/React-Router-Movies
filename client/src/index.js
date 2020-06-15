4bc920431a322ff8ffd4c857650a6bfaf879e45f
Mark Pekun <markpekun@Marks-MacBook-Pro.local>
2020-06-15 15:39:36 -0400

    fixed xcode errors.. 10 hours later..

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,10 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
-import { Router } from "express";
 
-<App />;
-ReactDOM.render(<Router></Router>, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
